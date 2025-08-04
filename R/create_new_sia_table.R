cb5b31ceb47576a6ef85ffe46657690df6d2ea2c
bigouetjp <bigouette.john@gmail.com>
2025-08-04 17:24:19 -0400

    SIA Table
    Built out coding - to get rows - need to upscale to identify when to start / stop on its own!

--- R/create_new_sia_table.R
+++ R/create_new_sia_table.R
@@ -2,7 +2,7 @@
 # Key Idea -> Use windows to identify SIAs build out subcomponets aligning with response to first SIA,
 # if there is any virus detected at the country after
 # Using Breakthrough window as 28 days from ending date of second SIA and following breakthrough
-source(here("R/create_obx_dataset.R"))
+source(here::here("R/create_obx_dataset.R"))
 
 # Load data
 raw.data <- sirfunctions::get_all_polio_data(size="medium")
@@ -18,7 +18,6 @@ x <- "ANG-cVDPV2-2"
 df_sub <- obx_table |> dplyr::filter(ob_id == x)
 
 # Global level inputs for table creations
-
 v_type <- c("cVDPV 2", "cVDPV 3", "cVDPV 1")
 v_type1 <- c("cVDPV 2", "cVDPV 3", "cVDPV 1",
              "cVDPV1andcVDPV2", "cVDPV2andcVDPV3","VDPV1andcVDPV2", "CombinationWild1-cVDPV 2")
@@ -26,6 +25,14 @@ start.date <- lubridate::as_date("2016-01-01")
 end.date <-   lubridate::floor_date(lubridate::today())
 date_end <- df_sub$sia_date_upper
 cov_pct_lvl <- 6
+
+ob_start <- df_sub$ob_srt_d0
+ob_end <- df_sub$most_recent
+sia_end <- df_sub$sia_date_upper
+ctry <- df_sub$ob_country
+sero <- df_sub$ob_type
+sia2 <- df_sub$sec_reg_sia_end
+b_virus <- df_sub$int_brk_vr
 
 
 # Load in Clean Global Data for viruses:
@@ -143,27 +150,15 @@ sia_sub2 <- sia_sub |>
 # Get col names in order on data set with master line
 # Find out how to loop through each
 
-
-
-x <- "ANG-cVDPV2-2"
 # x <- df_all$ob_id[i]
-df_sub <- obx_table |> dplyr::filter(ob_id == x)
 
 base <- df_sub |>
-  dplyr::select(ob_id, ob_type, ob_country, ob_srt_epid,
-                most_recent, first_reg_sia, sec_reg_sia, sec_reg_sia_end, int_brk_vr) |>
-  dplyr::mutate(ob_id_sia = paste0(ob_id, "-", "0"))
+  dplyr::ungroup() |>
+  dplyr::select(ob_id, ob_country, ipv_ctry, ob_srt_epid, ob_srt_onset, ob_srt_d0, sec_reg_sia, sec_reg_sia_end, most_recent, int_brk_vr)
 
-ob_start <- df_sub$ob_srt_d0
-ob_end <- df_sub$most_recent
-sia_end <- df_sub$sia_date_upper
-ctry <- df_sub$ob_country
-sero <- df_sub$ob_type
-sia2 <- df_sub$sec_reg_sia_end
-b_virus <- df_sub$int_brk_vr
 
 #Identify  the first breakthrough virus was after the breakthrough
-next_line <- positives.clean.01 |>
+next_virus <- positives.clean.01 |>
   dplyr::filter(
     place.admin.0 == ctry &
       measurement == sero &
@@ -171,9 +166,46 @@ next_line <- positives.clean.01 |>
   dplyr::arrange(report_date, dateonset) |>
   dplyr::select(epid, place.admin.0, place.admin.1, dateonset, report_date) |>
   dplyr::summarise(
-    ob_sia_ida = paste0(x, "-", "1"),
-    br_epid = dplyr::first(epid),
-    br_onset = dplyr::first(dateonset),
-    br_day0 = dplyr::first(report_date))
+    ob_id = x,
+    ob_country = dplyr::first(place.admin.0),
+    ob_srt_epid = dplyr::first(epid),
+    ob_srt_onset = dplyr::first(dateonset),
+    ob_srt_d0 = dplyr::first(report_date),
+    most_recent = last(dateonset))
+
+# Can also arrange on second col and merg variables here
+
+sia_next <- next_virus$ob_srt_d0
+
 
 # Bring in SIA Data
+next_sia <- sia_sub2 |>
+              dplyr::filter(sia_date >= sia_next) |>
+              dplyr::summarise(
+                sec_reg_sia = dplyr::first(sia_date),
+                sec_reg_sia_end = dplyr::first(sia_date_end))
+
+next_virus <- dplyr::bind_cols(next_virus, next_sia)
+
+b1 <- base |>
+        dplyr::select(ob_id, ipv_ctry)
+
+next_virus <- dplyr::left_join(next_virus, b1, by = "ob_id") |>
+  dplyr::relocate(ipv_ctry, .after = ob_country)
+next_virus <-    next_virus |>
+                 dplyr::mutate(int_brk_vr = dplyr::case_when(
+                   is.na(ipv_ctry) == T  & most_recent > (sec_reg_sia_end + lubridate::days(28)) ~ "1_y",
+                   is.na(ipv_ctry) == T  & most_recent <= (sec_reg_sia_end + lubridate::days(28)) ~ "2_n",
+                   is.na(ipv_ctry) == F ~ "5_ipvctry"))|>
+  dplyr::relocate(most_recent, .after = sec_reg_sia_end)
+
+##### Next part will need an if/else here for multiple ones
+
+br_sb <- dplyr::bind_rows(base, next_virus) |>
+           dplyr::mutate(subid = dplyr::row_number(),
+                         ob_sia_id = paste0(ob_id, "-", subid)) |>
+          dplyr::select(-subid)
+
+
+# Add in ID after, can bring in down after to make the id, combine with the first line after - add in the sublabels afterwards
+
