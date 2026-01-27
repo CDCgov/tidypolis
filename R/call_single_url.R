99a9b39946a3f2863aa2583bdd2477ecb16f4262
Mervin Keith Cuadera <40894971+mcuadera@users.noreply.github.com>
2026-01-27 14:34:38 -0500

    move call_single_url to its own script
    easier to maintain and test

---
+++ R/call_single_url.R
@@ -0,0 +1,53 @@
+#' Call single URL
+#'
+#' @description
+#' Call a return the formatted output from one URL.
+#'
+#' @param url `str` Single url.
+#' @param api_key `str` validated API key.
+#' @param times `int` Number of times to attempt connection with API.
+#' @export
+#' @returns tibble
+#' @examples
+#' \dontrun{
+#' call_single_url("https://extranet.who.int/polis/api/v2/Virus?$filter=VirusDate%20gt%202025-01-01")
+#' }
+#'
+call_single_url <- function(url,
+                            api_key = Sys.getenv("POLIS_API_KEY"),
+                            times = 10) {
+  # disable SSL Mode
+  httr::set_config(httr::config(ssl_verifypeer = 0L))
+
+  # response <- httr::GET(url=url, httr::add_headers("authorization-token" = api_key))
+  response <- httr::RETRY(
+    verb = "GET",
+    url = url,
+    config = httr::add_headers("authorization-token" = api_key),
+    times = times,
+    quiet = TRUE,
+    terminate_on_success = TRUE
+  )
+
+  out <- jsonlite::fromJSON(rawToChar(response$content))
+  value <- dplyr::as_tibble(out$value)
+  nextLink <- out$`@odata.nextLink`
+
+  while (!is.null(nextLink)) {
+    response <- httr::RETRY(
+      verb = "GET",
+      url = nextLink,
+      config = httr::add_headers("authorization-token" = api_key),
+      times = times,
+      quiet = TRUE,
+      terminate_on_success = TRUE
+    )
+
+    out <- jsonlite::fromJSON(rawToChar(response$content))
+    value <- dplyr::bind_rows(value, dplyr::as_tibble(out$value))
+    nextLink <- out$`@odata.nextLink`
+  }
+
+  return(value)
+
+}
--- R/utils.R
+++ R/utils.R
@@ -670,52 +670,6 @@ call_urls <- function(urls) {
   return(resp)
 }
 
-#' Call single URL
-#' @description Call a return the formatted output frome one URL
-#' @param url `str` single url
-#' @param api_key `str` validated API key
-#' @param times `int` number of times to attempt connection with API
-#' @export
-#' @returns tibble
-call_single_url <- function(url,
-                            api_key = Sys.getenv("POLIS_API_KEY"),
-                            times = 10) {
-  # disable SSL Mode
-  httr::set_config(httr::config(ssl_verifypeer = 0L))
-
-  # response <- httr::GET(url=url, httr::add_headers("authorization-token" = api_key))
-  response <- httr::RETRY(
-    verb = "GET",
-    url = url,
-    config = httr::add_headers("authorization-token" = api_key),
-    times = times,
-    quiet = TRUE,
-    terminate_on_success = TRUE
-  )
-
-  out <- jsonlite::fromJSON(rawToChar(response$content))
-  value <- dplyr::as_tibble(out$value)
-  nextLink <- out$`@odata.nextLink`
-
-  while (!is.null(nextLink)) {
-    response <- httr::RETRY(
-      verb = "GET",
-      url = nextLink,
-      config = httr::add_headers("authorization-token" = api_key),
-      times = times,
-      quiet = TRUE,
-      terminate_on_success = TRUE
-    )
-
-    out <- jsonlite::fromJSON(rawToChar(response$content))
-    value <- dplyr::bind_rows(value, dplyr::as_tibble(out$value))
-    nextLink <- out$`@odata.nextLink`
-  }
-
-  return(value)
-
-}
-
 #' Run single table diagnostic
 #'
 #' @description Run single table diagnostic
