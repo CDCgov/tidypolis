4294b1e4a9e19912e9a898872e3d4955155b6cc0
Mervin Keith Cuadera <40894971+mcuadera@users.noreply.github.com>
2026-01-29 10:37:48 -0500

    update get table ids to support parallel calls

--- R/get_table_ids.R
+++ R/get_table_ids.R
@@ -5,9 +5,10 @@
 #'
 #' @param table_data `tibble` One row tibble with the associated table data.
 #' @param api_key `str` POLIS API Key.
+#' @param parallel_calls `str` Whether to get table IDs using parallel calls.
 #' @returns `str` A vector of IDs.
 #' @export
-get_table_ids <- function(table_data, api_key = Sys.getenv("POLIS_API_KEY")) {
+get_table_ids <- function(table_data, api_key = Sys.getenv("POLIS_API_KEY"), parallel_calls = TRUE) {
     cli::cli_process_start(paste0("Downloading ", .table, " table IDs"))
 
     # disable SSL Mode
@@ -24,9 +25,10 @@ get_table_ids <- function(table_data, api_key = Sys.getenv("POLIS_API_KEY")) {
         table_data$polis_id
       )
 
-    urls <- create_table_urls(url, table_data, NULL)
+    day_intervals <- ifelse(parallel_calls, 365, NULL)
+    urls <- create_table_urls(url, table_data, days_intervals)
     response <- call_urls(urls)
-    ids <- urls |> dplyr::pull(table_data$polis_id)
+    ids <- response |> dplyr::pull(table_data$polis_id)
 
     cli::cli_process_done()
 
