86e0381224a743f9324223d17886d66ee95b07f2
Mervin Keith Cuadera <40894971+mcuadera@users.noreply.github.com>
2026-03-25 11:33:57 -0400

    draft parallel url requests using httr2
    allows us to call urls in parallel in posit workbench as it doesn't rely on the futures package and using background sessions nor cores for parallelization

---
+++ R/call_urls_in_parallel.R
@@ -0,0 +1,75 @@
+# Private function
+
+#' Helper function of call_urls_in_parallel
+#' 
+#' @description
+#' Does the actual requests to the POLIS API.
+#' 
+#' @inheritParams call_urls_in_parallel
+#'
+#' @returns `list` A list containing a tibble of data and a vector of odata nextLink values.
+#'
+#' @keywords internal
+#' 
+call_urls_in_parallel_helper <- function(urls, polis_key, requests_per_minute) {
+
+  url_requests <- purrr::map(urls, \(x) {
+    httr2::request(x) |>
+      httr2::req_headers(`Authorization-Token` = polis_key) |>
+      httr2::req_throttle(capacity = requests_per_minute)
+  })
+
+  response <- httr2::req_perform_parallel(url_requests)
+  out <- purrr::map(response, \(x) {
+    httr2::resp_body_json(x, simplifyVector = TRUE)
+  })
+
+  # the actual data
+  value <- purrr::map(out, \(x) x$value) |> 
+    dplyr::bind_rows() |>
+    dplyr::tibble()
+
+  # get the next links, remove the NULLs
+  next_link_urls <- purrr::map(out, \(x) x$`@odata.nextLink`) |> unlist()
+
+  return(list(data = value, next_links = next_link_urls))
+
+}
+
+# Main function
+
+#' Requests data from the POLIS API in parallel
+#' 
+#' @description
+#' Requests data from the POLIS API.
+#'
+#' @param urls `str` A URL string or a vector of URL strings to call to the POLIS API.
+#' @param requests_per_minute `int` Maximum number of requests per minute. Defaults to 30.
+#' @param polis_key `str` POLIS API key.
+#'
+#' @returns `tibble` Data requested from the POLIS API. 
+#'
+#' @export
+#' 
+#' @examples
+#' \dontrun{
+#'    init_tidypolis() # initializes saved POLIS API key in the global environment
+#'    urls <- c("https://extranet.who.int/polis/api/v2/Virus?$filter=UpdatedDate%20ge%202026-01-20T11:56:54.220Z%20and%20UpdatedDate%20le%202026-01-27T11:56:54.220Z",
+#'              "https://extranet.who.int/polis/api/v2/Virus?$filter=UpdatedDate%20gt%202026-01-27T11:56:54.220Z")
+#'    virus_data <- call_urls_in_parallel(urls)
+#' }
+call_urls_in_parallel <- function(urls, requests_per_minute = 30, polis_key = Sys.getenv("POLIS_API_KEY")) {
+
+  response <- call_urls_in_parallel_helper(urls, polis_key, requests_per_minute)
+  api_data <- response$data
+  next_link_urls <- response$next_links
+
+  while (length(next_link_urls) != 0) {
+    response <- call_urls_in_parallel_helper(next_link_urls, polis_key, requests_per_minute)
+    api_data <- dplyr::bind_rows(api_data, response$data)
+    next_link_urls <- response$next_links
+  }
+
+  return(api_data)
+
+}
