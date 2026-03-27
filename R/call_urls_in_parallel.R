020b609b8791bf38b71a5fac4efbe4e51ad6c451
Mervin Keith Cuadera <40894971+mcuadera@users.noreply.github.com>
2026-03-27 08:37:32 -0400

    format code via Posit Air

--- R/call_urls_in_parallel.R
+++ R/call_urls_in_parallel.R
@@ -1,19 +1,23 @@
 # Private function
 
 #' Helper function of call_urls_in_parallel
-#' 
+#'
 #' @description
-#' Does the actual requests to the POLIS API. The default is set to 10 concurrent requests, 
+#' Does the actual requests to the POLIS API. The default is set to 10 concurrent requests,
 #' which is the default of the [httr2::req_perform_parallel] function.
-#' 
+#'
 #' @inheritParams call_urls_in_parallel
 #'
 #' @returns `list` A list containing a tibble of data and a vector of odata nextLink values.
 #'
 #' @keywords internal
-#' 
-call_urls_in_parallel_helper <- function(urls, polis_key, requests_per_minute, concurrent_requests) {
-
+#'
+call_urls_in_parallel_helper <- function(
+  urls,
+  polis_key,
+  requests_per_minute,
+  concurrent_requests
+) {
   url_requests <- purrr::map(urls, \(x) {
     tryCatch(
       {
@@ -36,10 +40,12 @@ call_urls_in_parallel_helper <- function(urls, polis_key, requests_per_minute, c
     return(list(data = dplyr::tibble(), next_links = character()))
   }
 
-  response <- httr2::req_perform_parallel(url_requests, 
-    on_error = "continue", 
-    max_active = concurrent_requests)
-  
+  response <- httr2::req_perform_parallel(
+    url_requests,
+    on_error = "continue",
+    max_active = concurrent_requests
+  )
+
   out <- purrr::map(response, \(x) {
     tryCatch(
       {
@@ -49,12 +55,12 @@ call_urls_in_parallel_helper <- function(urls, polis_key, requests_per_minute, c
         cli::cli_alert_info(paste0("Bad request: ", x$url))
         NULL
       }
-    ) 
+    )
   }) |>
     purrr::compact()
 
   # the actual data
-  value <- purrr::map(out, \(x) x$value) |> 
+  value <- purrr::map(out, \(x) x$value) |>
     dplyr::bind_rows() |>
     dplyr::tibble()
 
@@ -62,13 +68,12 @@ call_urls_in_parallel_helper <- function(urls, polis_key, requests_per_minute, c
   next_link_urls <- purrr::map(out, \(x) x$`@odata.nextLink`) |> unlist()
 
   return(list(data = value, next_links = next_link_urls))
-
 }
 
 # Main function
 
 #' Requests data from the POLIS API in parallel
-#' 
+#'
 #' @description
 #' Requests data from the POLIS API.
 #'
@@ -77,10 +82,10 @@ call_urls_in_parallel_helper <- function(urls, polis_key, requests_per_minute, c
 #' @param polis_key `str` POLIS API key.
 #' @param concurrent_requests `int` Number of concurrent requests. Defaults to 10.
 #'
-#' @returns `tibble` Data requested from the POLIS API. 
+#' @returns `tibble` Data requested from the POLIS API.
 #'
 #' @export
-#' 
+#'
 #' @examples
 #' \dontrun{
 #'    init_tidypolis() # initializes saved POLIS API key in the global environment
@@ -88,18 +93,31 @@ call_urls_in_parallel_helper <- function(urls, polis_key, requests_per_minute, c
 #'              "https://extranet.who.int/polis/api/v2/Virus?$filter=UpdatedDate%20gt%202026-01-27T11:56:54.220Z")
 #'    virus_data <- call_urls_in_parallel(urls)
 #' }
-call_urls_in_parallel <- function(urls, polis_key = Sys.getenv("POLIS_API_KEY"), requests_per_minute = 30, concurrent_requests = 10) {
-  
-  response <- call_urls_in_parallel_helper(urls, polis_key, requests_per_minute, concurrent_requests)
+call_urls_in_parallel <- function(
+  urls,
+  polis_key = Sys.getenv("POLIS_API_KEY"),
+  requests_per_minute = 30,
+  concurrent_requests = 10
+) {
+  response <- call_urls_in_parallel_helper(
+    urls,
+    polis_key,
+    requests_per_minute,
+    concurrent_requests
+  )
   api_data <- response$data
   next_link_urls <- response$next_links
 
   while (length(next_link_urls) != 0) {
-    response <- call_urls_in_parallel_helper(next_link_urls, polis_key, requests_per_minute, concurrent_requests)
+    response <- call_urls_in_parallel_helper(
+      next_link_urls,
+      polis_key,
+      requests_per_minute,
+      concurrent_requests
+    )
     api_data <- dplyr::bind_rows(api_data, response$data)
     next_link_urls <- response$next_links
   }
 
   return(api_data)
-
 }
